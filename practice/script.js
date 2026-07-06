033f68337bfd2bb1b81dcd4936fb6d8e70646a39
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-07-06 17:20:37 +0300

    Finish the exercises.

--- practice/script.js
+++ practice/script.js
@@ -6,4 +6,29 @@ content.textContent = "This is the glorious text content";
 
 container.appendChild(content);
 
-// Exercise --> The Odin Project
+const paragraph = document.createElement("p");
+paragraph.style.color = "red";
+paragraph.textContent = "Hey I'am red!";
+container.appendChild(paragraph);
+
+const h3Title = document.createElement("h3");
+h3Title.style["color"] = "blue";
+h3Title.textContent = "I'm a blue h3!";
+container.appendChild(h3Title);
+
+const childDiv = document.createElement("div");
+childDiv.style.border = "black 1px solid";
+childDiv.style["background"] = "pink";
+
+const h1Title = document.createElement("h1");
+h1Title.textContent = "I'm in a div";
+
+const childParagraph = document.createElement("p");
+childParagraph.textContent = "ME TOO!";
+
+childDiv.appendChild(h1Title);
+childDiv.appendChild(childParagraph);
+
+container.appendChild(childDiv);
+
+// Events --> The Odin Project
