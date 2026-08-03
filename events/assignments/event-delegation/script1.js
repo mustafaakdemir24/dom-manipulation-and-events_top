a2c83f8743f2a1004a34ccade03cac6f17484863
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-08-03 16:20:37 +0300

    Finish 'Event delegation' chapter of Assignments section on The Odin Project.

---
+++ events/assignments/event-delegation/script1.js
@@ -0,0 +1,14 @@
+let home = document.querySelector("#home");
+home.addEventListener("click", (event) => {
+  console.log("Home menu item was clicked.");
+});
+
+let dashboard = document.querySelector("#dashboard");
+dashboard.addEventListener("click", (event) => {
+  console.log("Dashboard menu item was clicked.");
+});
+
+let report = document.querySelector("#report");
+report.addEventListener("click", (event) => {
+  console.log("Report menu item was clicked.");
+});
